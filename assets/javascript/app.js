8263dbc2b45c28652447a6edb263730ff99aea54
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-03-01 09:00:30 -0600

    addJSQuestions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,14 +2,65 @@
   //Generate questions Loop
   $(".start").on("click", function () {
     $(".start").remove();  
+    game.loadQuestion();
   })
 
 //Game Questions/Choices
-var questions = [{
-    q1: "What food does Carl eat out of a can while sitting on the roof of a house?",
-    choices: ["     Corn     ", "     Pudding     ", "     Beans     ", "     Applesauce     "],
-    correctAnswer: "Pudding",
-}];
+var questions = [
+  {
+    question1:
+      "What food does Carl eat out of a can while sitting on the roof of a house?",
+    choices: [
+        "Corn",
+        "Pudding",
+        "Beans",
+        "Applesauce"
+    ],
+    correct: "pudding"
+  },
+
+  {
+    question2: "What does Michonne stab the governor's eye with?",
+    choices: [
+      "Her Katana",
+      "A Shard of Glass",
+      "The Governor's Knife",
+      "A piece of Wood"
+    ],
+    correct: "A Shard of Glass"
+  },
+
+  {
+    question3: "Who tells Rick she also has talked to a deceased loved one?",
+    choices: [
+        "Beth",
+        "Andrea",
+        "Carol",
+        "Michonne"
+    ],
+    correct: "Michonne"
+  },
+  {
+    question4: "Who gives Carol a cherokee rose?",
+    choices: [
+        "Daryl",
+        "Dale",
+        "Sophia",
+        "Andrea"
+    ],
+    correct: "Daryl"
+  },
+  {
+    question5: "Who lets all the walkers out of the Greene's barn?",
+    choices: [
+        "Hershel",
+        "Carol",
+        "Rick",
+        "Shane"
+    ],
+    correct: "Shane"
+  }
+];
 
 var game ={
     questions:questions,
@@ -17,6 +68,7 @@ var game ={
     counter:25,
     correct:0,
     incorrect:0,
+
     countdown: function(){
         gameBoard.counter--;
         $(".counter").html(game.counter);
@@ -24,12 +76,14 @@ var game ={
             console.log("Out of Time! Try Again");
             game.timeUp();
         }
-
-
+        
     },
     loadQuestion: function(){
         timer =setInterval(game.countdown,2000);
         $('trivia').html('<h2>'+question[game.currentQuestion].question + '</h2>');
+        for (var i=0; i<questions[game.currentQuestion].answers.length;i++){
+            $('trivia').append('<button class="answer.button" id=button-'+i+'"data-name="'+questions[game.currentQuestion].answers[i]+'">'+questions[game.currentQuestion].answers[i]+'</button>');
+        }
     },
     nextQuestion: function () {
         
