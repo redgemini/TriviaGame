7341c550926385ea6db76960d89ec3a086354f46
redgemini <wendykiddy@Wendys-MacBook-Pro.local>
2018-02-28 21:32:07 -0600

    updateJS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,89 +1,70 @@
-//Pseudocoding
-//HW Instructions:  
-    //You'll create a trivia game that shows only one question until the player answers it or their time runs out.
-    //If the player selects the correct answer, show a screen congratulating them for choosing the right option.
-        //After a few seconds, display the next question -- do this without user input.
-    //The scenario is similar for wrong answers and time-outs.
-    //If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
-    //If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, 
-        //then show the next question.
-    //On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
-//Elements Needed:
-//Set of questions/choices
-//25 second timer for each question - next question populates when time runs out
-//click function to select correct answer
-//If statements:  
-    //If player is correct - 1) Show congratulatuons 2)Go to next question
-    //If player runs out of time - 1) Tell Player "Time is up" 2) Display correct answer 3) Go to next question
-    //If player is wrong - 1) Tell Player "Selected wrong answer" 2) Display correct
-//window.onload = function () {
-//$("#round").on("click", stopwatch.recordround);
-//$("#reset").on("click", stopwatch.reset);
-//$("#start").on("click", stopwatch.start);
-//};
+//-----Functions-----
+  //Generate questions Loop
+  $(".start").on("click", function () {
+    $(".start").remove();  
+  })
 
-
-//Set of questions/choices
-
-$(".start").on("click", function() {
-        //Generate questions Loop
-    for(var i=0;i<questions.length;i++){
-        $(".trivia").append("<h2>" + questions[i].question+"</h2>");
-        //Generate choices loop with Radio button
-    for (var a=0;a<questions[i].choices.length;a++) {
-             $(".trivia").append("<input type='radio' name='question-"+i+"'value='"+questions[i].choices[a]+"'>"+questions[i].choices[a])
-         }
-        }
-})
-
+//Game Questions/Choices
 var questions = [{
-        question: "What food does Carl eat out of a can while sitting on the roof of a house?",
-        choices: ["Corn", "Pudding", "Beans", "Applesauce"],
-        correctAnswer: 1
+    q1: "What food does Carl eat out of a can while sitting on the roof of a house?",
+    choices: ["     Corn     ", "     Pudding     ", "     Beans     ", "     Applesauce     "],
+    correctAnswer: "Pudding",
+}];
+
+var game ={
+    questions:questions,
+    currentQuestion:0,
+    counter:25,
+    correct:0,
+    incorrect:0,
+    countdown: function(){
+        gameBoard.counter--;
+        $(".counter").html(game.counter);
+        if(game.counter===0){
+            console.log("Out of Time! Try Again");
+            game.timeUp();
+        }
+
+
     },
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: 0
+    loadQuestion: function(){
+        timer =setInterval(game.countdown,2000);
+        $('trivia').html('<h2>'+question[game.currentQuestion].question + '</h2>');
     },
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: 0
+    nextQuestion: function () {
+        
     },
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: 0
+    timeUp: function (){
+
     },
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: 0
+    results: function (){
+
     },
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: 0
+
+    clicked: function (){
+
     },
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: 0
+
+    answerCorrect: function(){
+
     },
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: 0
+
+    answerIncorrect: function(){
+
     },
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: 0
+
+    reset: function(){
+
     },
-    {
-        question: "",
-        choices: ["", "", "", ""],
-        correctAnswer: 0
-    },
-]
+
+}
+
+
+
+
+//--Console.log Testing--
+console.log(questions);
+console.log(questions.q1);
+
+
+
